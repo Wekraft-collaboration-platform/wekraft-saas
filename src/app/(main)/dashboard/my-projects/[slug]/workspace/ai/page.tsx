02aef253f2847fc3c1a6b4567674cad49ce222e1
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2026-04-21 14:30:06 +0530

    kaya svg added....

--- src/app/(main)/dashboard/my-projects/[slug]/workspace/ai/page.tsx
+++ src/app/(main)/dashboard/my-projects/[slug]/workspace/ai/page.tsx
@@ -1,8 +1,12 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 const AIWorkspace = () => {
-  return <div>AIWorkspace</div>;
+  return <div className="h-screen w-full bg-background">
+    <Image src="/kaya.svg" alt="AI" width={50} height={50} />
+    <h2 className="text-2xl font-semibold font-pop">Kaya</h2>
+  </div>;
 };
 
 export default AIWorkspace;
